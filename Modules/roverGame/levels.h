9e0d57f1ac59aa1d534ae8517f611e2e192f2aa0
ikumar21 <ikumar@scu.edu>
2024-05-07 10:22:36 -0700

    more realistic levels

--- Modules/roverGame/levels.h
+++ Modules/roverGame/levels.h
@@ -7,16 +7,17 @@ typedef struct{
   
 }Level_Info;
 
-Level_Info lvl1Info = {{2000, 4000}, 20, 3, 25};
-Level_Info lvl2Info = {{3000, 5000}, 10, 45, 50};
-Level_Info lvl3Info = {{3000, 5000}, 10, 60, 300};
-Level_Info lvl4Info = {{3000, 5000}, 8, 45, 200};
-Level_Info lvl5Info = {{3000, 5000}, 8, 60, 300};
-Level_Info lvl6Info = {{3000, 5000}, 5, 60, 200};
-Level_Info lvl7Info = {{3000, 5000}, 20, 60, 400};
-Level_Info lvl8Info = {{3000, 5000}, 3, 30, 50};
-Level_Info lvl9Info = {{3000, 5000}, 2, 45, 50};
-Level_Info lvl10Info = {{3000, 5000}, 1, 30, 10};
+//                      min, max, maxCoin, gameTime, targetScore
+Level_Info lvl1Info = {{100, 200}, 20, 60, 50};
+Level_Info lvl2Info = {{100, 200}, 15, 45, 45};
+Level_Info lvl3Info = {{400, 600}, 20, 60, 75};
+Level_Info lvl4Info = {{300, 500}, 8, 45, 35};
+Level_Info lvl5Info = {{1000, 2000}, 7, 60, 30};
+Level_Info lvl6Info = {{1000, 1500}, 5, 60, 35};
+Level_Info lvl7Info = {{200, 300}, 20, 60, 125};
+Level_Info lvl8Info = {{50, 100}, 1, 15, 8};
+Level_Info lvl9Info = {{500, 600}, 2, 45, 15};
+Level_Info lvl10Info = {{50, 100}, 1, 30, 10}; 
 
 Level_Info *allLvlInfo[10] =  {&lvl1Info, &lvl2Info, &lvl3Info, &lvl4Info, 
                                &lvl5Info, &lvl6Info, &lvl7Info, &lvl8Info,
